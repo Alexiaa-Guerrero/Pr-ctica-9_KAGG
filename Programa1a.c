51c972d4ed6a28f119c0c8a81ec493a3a97120f5
Alexiaa-Guerrero <karlaalexia.guerrerogonzalez@gmail.com>
2024-11-09 04:06:01 -0600

    Create Programa1a.c

---
+++ Programa1a.c
@@ -0,0 +1,20 @@
+/*Karla Alexia Guerrero González
+Práctica 9 11/10/24
+Programa 1a correspondiente a la práctica*/
+
+
+#include <stdio.h>
+int main ()
+ {
+ int lista[5] = {10, 8, 5, 8, 7};
+ int indice = 0;
+ printf("\tLista\n");
+ while (indice < 5 ) 
+ {
+ printf("\nCalificación del alumno %d es %d", indice+1, lista[indice]);
+ indice += 1;
+ }
+
+ printf("\n");
+ return 0;
+ }
